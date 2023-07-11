6ca2932ab5b210391d1c416d53696ab3df0114b3
suyelu <suyelu@hotmail.com>
2023-07-11 15:47:08 +0800

    添加了头部注释，包含作者信息，文件基本信息等

--- init.lua
+++ init.lua
@@ -746,3 +746,47 @@ vim.api.nvim_create_autocmd(
     group = group
   }
 )
+
+
+
+-- 为源代码添加作者，邮箱，时间等信息
+vim.cmd([[
+autocmd BufNewFile *.cpp,*.[ch],*.sh,*.java,*.go exec ":call SetTitle()"
+"""定义函数SetTitle，自动插入文件头
+func SetTitle()
+    "如果文件类型为.sh文件
+    if &filetype == 'sh'
+        call setline(1,"\#########################################################################")
+        call append(line("."),   "\# File Name:    ".expand("%"))
+        call append(line(".")+1, "\# Author:       suyelu")
+        call append(line(".")+2, "\# mail:         suyelu@hotmail.com")
+        call append(line(".")+3, "\# Created Time: ".strftime("%c"))
+        call append(line(".")+4, "\#########################################################################")
+        call append(line(".")+5, "\#!/bin/bash")
+        call append(line(".")+6, "")
+    else
+        call setline(1, "/* ************************************************************************")
+        call append(line("."),   "> File Name:     ".expand("%"))
+        call append(line(".")+1, "> Author:        suyelu")
+        call append(line(".")+2, "> mail:          suyelu@hotmail.com")
+        call append(line(".")+3, "> Created Time:  ".strftime("%c"))
+        call append(line(".")+4, "> Description:   ")
+        call append(line(".")+5, " ************************************************************************/")
+        call append(line(".")+6, "")
+    endif
+    "新建文件后，自动定位到文件末尾
+    autocmd BufNewFile * normal G
+endfunc
+]])
+
+vim.cmd([[
+autocmd BufNewFile *.h,*.hpp exec ":call AddHHeader()"
+func AddHHeader()
+    let macro = "_".toupper(substitute(expand("%"), "[/.]", "_", "g"))."_"
+    "normal o
+    call setline(9, "#ifndef ".macro)
+    call setline(10, "#define ".macro)
+    call setline(11, "")
+    call setline(12, "#endif  // ".macro)
+endfunc
+]])
