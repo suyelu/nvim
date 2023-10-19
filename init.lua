b972d14daaf52f4aeeb632568beb67fed6133952
suyelu <suyelu@hotmail.com>
2023-10-19 15:34:53 +0800

    update

--- init.lua
+++ init.lua
@@ -141,8 +141,8 @@ require('lazy').setup({
     -- See `:help indent_blankline.txt`
     main = "ibl",
     opts = {
-      char = '┊',
-      show_trailing_blankline_indent = false,
+      --char = '┊',
+      --show_trailing_blankline_indent = false,
     },
   },
 
